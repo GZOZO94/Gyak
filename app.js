5da37fb84844d3223301a9b27039bfb05f8c76ca
GZOZO94 <gaszol11.c@gmail.com>
2017-03-16 18:04:05 +0100

    34

---
+++ app.js
@@ -0,0 +1,25 @@
+/*var http=require('http');
+
+var server = http.createServer(function(request,response){
+	response.writeHead(200, {"Content-Type":"text/plain"});
+	response.end("Hello node server!");
+});
+
+server.listen(4242);*/
+var express=require('express'),
+	app=express();
+	
+app.get("/", function(request,response){
+	response.sendfile("index.html");
+});
+
+app.get("/login", function(request,response){
+	response.sendfile("login.html");
+});
+
+app.get("/:user/send", function(request,response){
+	response.send("send"+request.params.user + "     "+ request.query.query);
+});
+
+app.listen(8080);
+console.log('Server is running');
